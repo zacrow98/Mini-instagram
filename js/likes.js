85653132dc8e9104aaa0fe4621df45a9ff9c5831
zacrow98 <agustindeluca1998@gmail.com>
2022-10-11 18:03:46 -0300

    Casiterminado

---
+++ js/likes.js
@@ -0,0 +1,46 @@
+
+<script> {
+        //    First Like Button   
+    var btnvar1 = document.getElementById('btnh1');
+
+    function Toggle1(){
+            if (btnvar1.style.color =="red") {
+                btnvar1.style.color = "grey"
+            }
+            else{
+                btnvar1.style.color = "red"
+            }
+    }
+
+
+
+    //    Second Like Button   
+    var btnvar2 = document.getElementById('btnh2');
+
+    function Toggle2(){
+            if (btnvar2.style.color =="red") {
+                btnvar2.style.color = "grey"
+            }
+            else{
+                btnvar2.style.color = "red"
+            }
+    }
+
+
+
+
+    //    Third Like Button   
+    var btnvar3 = document.getElementById('btnh3');
+
+    function Toggle3(){
+            if (btnvar3.style.color =="red") {
+                btnvar3.style.color = "grey"
+            }
+            else{
+                btnvar3.style.color = "red"
+            }
+    }
+
+}
+
+</script>
